5958aaffcccb0755ac55c6f4e31754e3979448a7
zensgit <77236085+zensgit@users.noreply.github.com>
2025-09-19 16:58:21 +0800

    chore(flutter): phase 1 analyzer cleanup (remove unused import; add material import for Icons)

--- jive-flutter/lib/services/invitation_service.dart
+++ jive-flutter/lib/services/invitation_service.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import '../models/invitation.dart';
 import '../models/family.dart' as family_model;
 
--- jive-flutter/lib/services/social_auth_service.dart
+++ jive-flutter/lib/services/social_auth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 import '../utils/constants.dart';
 import 'auth_service.dart';
 
