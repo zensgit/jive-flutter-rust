54c4866d2e47aa147980cd54f104056dafefb401
zensgit <77236085+zensgit@users.noreply.github.com>
2025-09-30 16:51:09 +0800

    flutter: QR widget cleanup + SharePlus usage in invite dialog (#79)
    * flutter: cleanup QR widget stubs, fix const eval, use SharePlus.instance.share with ShareParams; add cross_file import
    
    * flutter: tidy qr_code_generator structure; remove cross_file import and leftover stub; fix const usage; migrate invite dialog share to SharePlus.instance

--- jive-flutter/lib/widgets/qr_code_generator.dart
+++ jive-flutter/lib/widgets/qr_code_generator.dart
@@ -198,7 +198,7 @@ class _QrCodeGeneratorState extends State<QrCodeGenerator>
                   width: widget.size,
                   height: widget.size,
                   child: const Center(
-                    child: const CircularProgressIndicator(),
+                    child: CircularProgressIndicator(),
                   ),
                 )
               : ScaleTransition(
@@ -297,35 +297,6 @@ class _QrCodeGeneratorState extends State<QrCodeGenerator>
           ),
         ],
       ],
-    );
-  }
-
-  // Stub methods for missing external dependencies
-  dynamic XFile(String path) {
-    return _StubXFile(path);
-  }
-
-  Widget QrImageView({
-    required String data,
-    dynamic version,
-    double? size,
-    Color? backgroundColor,
-    Color? foregroundColor,
-    dynamic errorCorrectionLevel,
-    dynamic embeddedImage,
-    double? embeddedImageSizeRatio,
-    EdgeInsets? padding,
-  }) {
-    return Container(
-      width: size ?? 200,
-      height: size ?? 200,
-      color: backgroundColor ?? Colors.white,
-      child: Center(
-        child: Text(
-          'QR Code Placeholder',
-          style: TextStyle(color: foregroundColor ?? Colors.black),
-        ),
-      ),
     );
   }
 }
@@ -479,12 +450,12 @@ class InvitationQrCodeDialog extends StatelessWidget {
                     icon: const Icon(Icons.share),
                     label: const Text('分享'),
                     onPressed: () async {
-                      await Share.share(
+                      await SharePlus.instance.share(ShareParams(text: 
                         '邀请你加入家庭「$familyName」\n\n'
                         '邀请码：$inviteCode\n'
                         '点击链接加入：$inviteLink\n\n'
                         '有效期：$daysLeft 天',
-                      );
+                      ));
                     },
                   ),
                 ),
@@ -540,9 +511,3 @@ class _InfoRow extends StatelessWidget {
   }
 }
 
-
-// Stub implementation for XFile
-class _StubXFile {
-  final String path;
-  _StubXFile(this.path);
-}
